5bedc535abdadfe3130d439a4cfd77ddb09734c0
Manas <prometheansacrifice@gmail.com>
2020-01-05 21:38:48 +0530

    Package manager setup (#1)
    * Utils added and tested
    
    * Refactor: project structure now includes path
    
    * Adds esy/command-env--to-process-environment
    
    * Adds esy/command-env--get-exec-path
    
    * bug fixes
    
    * Bsb/npm support: by dropping esy.json
    
    TODO: add @esy-ocaml/reason only if needed. TODO document
    
    * Adds docs
    
    * Comparison with 'esy command-exec'
    
    * De-activate for opam projects
    
    * Hook onto tuareg and caml modes too
    
    * Note about tools not being shipped.
    
    * Add a tip for quick bootstrapping of esy projects
    
    * Note about dormancy for opam
    
    * Fix typos

--- esy-mode.el
+++ esy-mode.el
@@ -29,15 +29,223 @@
   :link '(url-link :tag "github"
 		   "https://github.com/prometheansacrifice/esy-mode"))
 
+(setq lexical-binding t)
+
+(defvar esy-lsp-command "")
+(make-local-variable 'esy-lsp-command)
+(defvar esy-refmt-command nil)
+(make-local-variable 'esy-refmt-command)
+(defvar esy-merlin-command nil)
+(make-local-variable 'esy-merlin-command)
+(defvar esy-compile-command nil)
+(make-local-variable 'esy-compile-command)
+
+
 ;; Units
 (defun add-two (p) 
   (+ p 2))
 
+(defun esy/f--read (file-path)
+  "Return file content."
+  (with-temp-buffer
+    (insert-file-contents file-path)
+    (buffer-string)))
+
+(defun esy/f--write (fname data)
+  "Write to file"
+  (with-temp-file fname (insert data)))
+
+(defun esy/internal--project--get-manifest-file-path
+    (esy-status-json)
+  "Given the json object of 'esy status' output,
+it returns the manifest file"
+  (gethash "rootPackageConfigPath" esy-status-json))
+
 (defun esy/project--of-path (project-path) 
-  "of-path(path): returns an abstract structure that can later be used to
-                 obtain more info about the project"
+  "Returns an abstract structure that can later
+be used to obtain more info about the project"
   (let* ((default-directory project-path)
 	 (json-str (shell-command-to-string "esy status")) 
+	 (json-array-type 'list) 
+	 (json-key-type 'string) 
+	 (json-false 'nil)
+	 (json-object-type 'hash-table)
+	 (esy-status-json
+	  (condition-case nil 
+	      (json-read-from-string json-str)
+	    (error (progn
+		     (message "Error while json parsing \
+'esy status'")
+		     (make-hash-table)))))) 
+    (list 'json esy-status-json
+	  'path (let* ((manifest-path (esy/internal--project--get-manifest-file-path esy-status-json)))
+		  (if manifest-path
+		      (file-name-directory manifest-path)
+		    nil)))))
+
+(defun esy/project--get-path (project)
+  "Returns the root of the project"
+  (plist-get project 'path))
+
+(defun esy/project--get-manifest-file-path (project)
+  "Returns the path to manifest file"
+  (esy/internal--project--get-manifest-file-path
+   (plist-get project 'json)))
+
+(defun esy/project--of-file-path (file-path)
+  "Returns an abstract structure that can
+later be used to obtain more info about the esy project"
+  (let* ((project-path (file-name-directory file-path))) (progn (esy/project--of-path project-path))))
+
+(defun esy/project--of-buffer (buffer)
+  "Returns an abstract structure that can 
+later be used to obtain more info about the esy project"
+  (let* ((file-name (buffer-file-name buffer))) (if file-name (esy/project--of-file-path file-name) nil)))
+
+(defun esy/project--ready-p (project)
+  "Returns if a given project is ready for
+development ie. if the tools can be looked in it's sandbox"
+  (gethash "isProjectReadyForDev" (plist-get project 'json)))
+
+(defun esy/project--p (project) 
+  "Returns if a given project structure is a valid esy project"
+  (gethash "isProject" (plist-get project 'json)))
+
+(defun esy/command-env--of-project (project)
+  "Given a project, it returns an abstract structure
+command-env"
+  (let*
+      ((project-path (esy/project--get-path project))
+       (default-directory project-path)
+       (json-str
+	(condition-case
+	    nil
+	    (shell-command-to-string
+	     "esy command-env --json")
+	  (error (progn
+		   (message
+		    "Error while running 'esy command-env --json'")
+		   "{}"))))
+	 (json-array-type 'list)
+	 (json-key-type 'string)
+	 (json-false 'nil)
+	 (json-object-type 'hash-table)
+	 (esy-command-env-json
+	  (json-read-from-string json-str)))
+    (list 'command-env esy-command-env-json)))
+
+(defun esy/command-env--to-process-environment (command-env)
+  "Given a command-env, it turns it into a list
+that can be assigned to 'process-environment"
+  (let ((command-env-json
+	 (plist-get command-env 'command-env))
+	(penv '()))
+    (progn
+      (maphash (lambda (k v)
+		 (setq penv (cons (format "%s=%s" k v) penv)))
+	       command-env-json)
+      penv)))
+
+(defun esy/command-env--get-exec-path (command-env)
+  "Given a command-env, it turns it into a list that
+can be assigned to 'exec-path"
+  (let* ((penv
+	  (esy/command-env--to-process-environment
+	   command-env))
+	 (exec-path-list '())
+	 (path-env-str-list (seq-filter
+			(lambda (s) (string-match "^path" s))
+			penv)))
+    (progn
+      (dolist (e path-env-str-list)
+	(let* ((parts (split-string e "=")))
+	  (progn
+	    (if (eq (length parts) 2)
+		(progn
+		  (setq exec-path-list
+			(split-string
+			 (nth 1 parts)
+			 (if (string=
+			      system-type
+			      "windows-nt") ";" ":"))))))))
+      exec-path-list)))
+
+(defun esy/setup--esy-get-available-tools (project)
+  
+  "setup--esy-return-missing-tools(project): looks into the 
+esy sandbox and returns a plist of missing tools. Specifically,
+it looks for 
+
+1. refmt/ocamlfmt
+2. lsp
+3. merlin
+
+"
+  (let* ((command-env (esy/command-env--of-project project))
+	(tools '()))
+    (progn
+      (make-local-variable 'process-environment)
+      (setq process-environment
+	    (esy/command-env--to-process-environment
+	     command-env))
+      (make-local-variable 'exec-path)
+      (setq exec-path
+	    (esy/command-env--get-exec-path command-env))
+      (setq tools (plist-put tools 'build "esy"))
+      (setq tools (plist-put tools 'refmt (executable-find "refmt")))
+      (setq tools (plist-put tools 'merlin (executable-find "ocamlmerlin")))
+      (setq tools (plist-put tools 'lsp (executable-find "ocamllsp")))
+      tools)))
+
+(defun esy/setup--esy (project callback)
+  "setup--esy(project): runs ops to ensure project is ready
+for development"
+  (if (esy/project--ready-p project)
+      (progn
+	(message "Project ready for development")
+	(funcall callback
+		 (esy/setup--esy-get-available-tools project)))
+    (progn
+	  (add-hook
+	   'compilation-finish-functions
+	   (lambda (buffer desc)
+	     (funcall callback (esy/setup--esy-get-available-tools project))))
+	  (compile "esy"))))
+
+(defun esy/setup--opam (project callback)
+  "setup--opam(_): currently doesn't do anything. opam-user-setup works well enough, IMO!"
+  (message "Detected an opam project. Staying dormant"))
+
+(defun esy/setup--npm(project callback)
+  
+  "setup--npm(project): Although named 'npm', this function uses esy to setup the Reason/OCaml toolchain.
+
+npm is incapable of
+  a) handling prebuilts correctly
+  b) Correctly setup environment for tools that assume they are the only ones running. 
+     Eg: merlin expected the correct ocamlmerlin-reason available on it's path. This can be tricky in non-sandboxed setup where a user could have almost any version of ocamlmerlin installed
+
+"
+  (if (y-or-n-p "Seems like an npm/bsb project. It is recommended that you we drop and esy.json for you. Go ahead?")
+      (progn
+	(esy/f--write
+	 (concat
+	  (file-name-as-directory
+	   (esy/project--get-path project))
+	  "esy.json")
+	 "{
+ \"dependencies\": {
+    \"ocaml\": \"4.6.x\",
+    \"@esy-ocaml/reason\": \"*\",
+    \"@opam/ocaml-lsp-server\": \"ocaml/ocaml-lsp:ocaml-lsp-server.opam#e5e6ebf9dcf157\"
+  }
+}")
+	(esy/setup--esy project callback))))
+
+(defun esy/manifest--of-path (file-path)
+  "Creates an abstract manifest structure given file path"
+  (if (esy/manifest--json-p file-path)
+      (let ((json-str (esy/f--read file-path)) 
 	 (json-array-type 'list) 
 	 (json-key-type 'string) 
 	 (json-false 'nil)
@@ -47,26 +255,53 @@
 	(json-read-from-string json-str)
       (error (progn
 	       (message "Error while json parsing")
-	       (make-hash-table)))))))
+	       nil))))
+    (progn
+      (message "Non JSON manifest not supported yet")
+      nil))))
 
-(defun esy/project--of-file-path (file-path)
-  "of-file-path(path): returns an abstract structure that can later be used
-                       to obtain more info about the esy project"
-  (let* ((project-path (file-name-directory file-path))) (progn (esy/project--of-path project-path))))
+(defun esy/manifest--json-p (file-path)
+  "Takes a file path and returns if file at said path is 
+json or not"
+  ;; Cheat!
+  (if file-path (string-match "\.json$" file-path) nil))
 
-(defun esy/project--of-buffer (buffer)
-  "of-buffer(buffer): returns an abstract structure that can later be used 
-                      to obtain more info about the esy project"
-  (let* ((file-name (buffer-file-name buffer))) (if file-name (esy/project--of-file-path file-name) nil)))
+(defun esy/manifest--package-json-p (file-path)
+  "Takes a file path and returns if file at said path is 
+package.json or not"
+  (if file-path (string-match "package\.json$" file-path) nil))
 
-(defun esy/project--ready-p (json)
-  "ready-p(project): returns if a given project is ready for development ie.
-                     if the tools can be looked in it's sandbox"
-  (gethash "isProjectReadyForDev" json))
+(defun esy/manifest--contains-esy-field-p (manifest)
+  "Checks if a manifest structure contains esy field"
+  (if manifest (gethash "esy" manifest) nil))
 
-(defun esy/project--p (json) 
-  "ready-p(project): returns if a given project structure is a valid esy project"
-  (gethash "isProject" json))
+(defun esy/package-manager--of-project (project)
+  "Detect the package manager of the project. Returns either
+'esy|'opam|'npm"
+  (let* ((manifest-file-path
+	  (esy/project--get-manifest-file-path project)))
+	 (if (esy/manifest--json-p manifest-file-path)
+	     ;; The manifest file is a json.
+	     (if (esy/manifest--package-json-p
+		  manifest-file-path)
+		 ;; Could be npm or esy
+		 (if (esy/project--ready-p project)
+		     ;; esy says this project with package.json
+		     ;; is ready for development i.e. all it's
+		     ;; dependencies were fetched and installed
+		     ;; by esy. Definitely an esy project
+		     'esy
+		   (progn
+		     ;; Checking if there is an esy field in
+		     ;; the package.json. If there is one,
+		     ;; it's an esy project
+		     (if (esy/manifest--contains-esy-field-p
+			  (esy/manifest--of-path manifest-file-path))
+			 'esy
+		       'npm)))
+	       'esy)
+	   'opam)))
+
 
 ;;;###autoload
 (define-minor-mode esy-mode () 
@@ -74,21 +309,59 @@
   :lighter " esy"
   (progn
     ;; TODO: Check if esy is available on the system
-    (make-local-variable 'compile-command) 
-    (setq compile-command "esy")
     (let* ((project (esy/project--of-buffer (current-buffer))))
       (if (esy/project--p project)
 	  (progn
-	    (message "Activating esy-mode...")
-	    (if (esy/project--ready-p project)
-		(message "Project ready for development")
-	      (if (y-or-n-p
-		   "Seems like a valid esy project. Go ahead and install and build all dependencies?")
-		  (compile "esy") t)))
+	    
+	    ;;All npm and opam projects are valid esy projects
+	    ;;too! Picking the right package manager is important
+	    ;;- we don't want to run `esy` for a user who never
+	    ;;intended to. Example: bsb/npm users. Similarly,
+	    ;;opam users wouldn't want prompts to run `esy`. Why
+	    ;;is prompting `esy i` even necessary in the first
+	    ;;place? `esy ocamlmerlin-lsp` needs projects to
+	    ;;install/solve deps
+
+	    (let ((config-plist
+		   (let* ((project-type
+			  (esy/package-manager--of-project
+			   project))
+			 (callback
+			  (lambda (config-plist)
+			    (progn
+			      (setq-local esy-compile-command
+					  (plist-get
+					   config-plist
+					   'build))
+			      (setq-local esy-refmt-command
+					  (plist-get
+					   config-plist
+					   'refmt))
+			      (setq-local esy-merlin-command
+					  (plist-get
+					   config-plist
+					   'merlin))
+			      (setq-local esy-lsp-command
+					  (plist-get
+					   config-plist
+					   'lsp))
+			      ))))
+		     (cond ((eq project-type 'opam)
+			    (esy/setup--opam project
+					     callback))
+			    ((eq project-type 'esy)
+			     (esy/setup--esy project
+					     callback))
+			    ((eq project-type 'npm)
+			     (esy/setup--npm project
+					     callback))))))
+	      ))
 	(message "Doesn't look like an esy project. esy-mode will stay dormant")))))
 
 ;;;###autoload
 (add-hook 'reason-mode-hook 'esy-mode)
+(add-hook 'reason-mode-hook 'tuareg-mode)
+(add-hook 'reason-mode-hook 'caml-mode)
 
 (provide 'esy-mode)
 ;;; esy.el ends here
--- tests/esy.el
+++ tests/esy.el
@@ -2,24 +2,83 @@
 (require 'esy-mode)
 
 ;; Test utils
-(defun ert/f--write (fname data)
+(defun esy-test-utils/f--write (fname data)
   (with-temp-file fname (insert data)))
 
-
+(setq safe-guard nil)
 (defun ert/test-suite 
     (&rest 
      test-config
      ) 
   (let ((setup (plist-get test-config 
-			  :setup)) 
+			  :setup))
 	(body (plist-get test-config 
-			 :body)) 
-	(tear-down (plist-get test-config 
-			      :tear-down))) 
-    (progn (let* ((setup-result (funcall setup))) 
-	     (funcall body setup-result) 
-	     (funcall tear-down setup-result)))))
+			 :body))	
+	(teardown (plist-get test-config 
+			     :teardown))) 
+    (progn (let* ((setup-result
+		   (funcall setup (getenv "TMPDIR"))))
+	     (if safe-guard
+	     (condition-case err
+		 (funcall body setup-result)
+	    
+	       (error (progn
+			(funcall teardown setup-result)
+			(signal (car err) (cdr err))
+			)))
+	     (funcall body setup-result))
+	     (funcall teardown setup-result)))))
 
+(defun esy-test-utils/fixture--create (tmp-dir)
+  (let* ((test-esy-project-dir
+	  (concat
+	   (file-name-as-directory tmp-dir)
+	   "test-esy-project"))
+	 (test-esy-project-manifest
+	  (concat
+	   (file-name-as-directory test-esy-project-dir)
+	   "esy.json")))
+    (progn
+      (delete-directory test-esy-project-dir t)
+      (make-directory test-esy-project-dir)
+      (esy-test-utils/f--write
+       test-esy-project-manifest
+       "{ \"dependencies\": {} }")
+      test-esy-project-dir)))
+
+(defun esy-test-utils/fixture--create-npm (tmp-dir)
+  (let* ((test-esy-project-dir
+	  (concat
+	   (file-name-as-directory tmp-dir)
+	   "test-esy-project"))
+	 (test-esy-project-manifest
+	  (concat
+	   (file-name-as-directory test-esy-project-dir)
+	   "package.json")))
+    (progn
+      (delete-directory test-esy-project-dir t)
+      (make-directory test-esy-project-dir)
+      (esy-test-utils/f--write
+       test-esy-project-manifest
+       "{ \"dependencies\": {} }")
+      test-esy-project-dir)))
+
+(defun esy-test-utils/fixture--create-opam (tmp-dir)
+  (let* ((test-esy-project-dir
+	  (concat
+	   (file-name-as-directory tmp-dir)
+	   "test-esy-project"))
+	 (test-esy-project-manifest
+	  (concat
+	   (file-name-as-directory test-esy-project-dir)
+	   "foo.opam")))
+    (progn
+      (delete-directory test-esy-project-dir t)
+      (make-directory test-esy-project-dir)
+      (esy-test-utils/f--write
+       test-esy-project-manifest
+       "name: \"foo\"")
+      test-esy-project-dir)))
 
 (ert-deftest 
     test-add-two
@@ -27,75 +86,225 @@
   "Add two must do so"
   (should (equal (add-two 5) 7)))
 
+(ert-deftest
+    test-esy/manifest--json-p
+    ()
+  "must return if file at path is json or not"
+  (should (esy/manifest--json-p "/foo/bar/package.json"))
+  (should (not (esy/manifest--json-p "/foo/opam")))
+  (should (not (esy/manifest--json-p "/foo/foo.opam"))))
+
+(ert-deftest
+    test-esy/package-manager--of-project-when-esy
+    ()
+  "package-manager--of-project must return correct project type"
+  (ert/test-suite
+   :setup (lambda (tmp-dir) (esy-test-utils/fixture--create tmp-dir))
+   :body (lambda (test-project-path)
+	   (let ((test-project (esy/project--of-path test-project-path)))
+	   (should (eq
+		    (esy/package-manager--of-project test-project)
+		    'esy))))
+   :teardown (lambda (x) (delete-directory x t))))
+
+(ert-deftest
+    test-esy/package-manager--of-project-when-opam
+    ()
+  "package-manager--of-project must properly detect an opam project
+with an opam file"
+  (ert/test-suite
+   :setup (lambda (tmp-dir)
+	    (esy-test-utils/fixture--create-opam tmp-dir))
+   :body (lambda (test-project-path)
+	   (let ((test-project
+		  (esy/project--of-path test-project-path)))
+	   (should (eq
+		    (esy/package-manager--of-project test-project)
+		    'opam))))
+   :teardown (lambda (x) (delete-directory x t))))
+
+(ert-deftest
+    test-esy/package-manager--of-project-when-npm
+    ()
+  "package-manager--of-project must properly detect an npm 
+project with a package.json (but no esy field in it)"
+  (ert/test-suite
+   :setup (lambda
+	    (tmp-dir)
+	    (esy-test-utils/fixture--create-npm tmp-dir))
+   :body (lambda
+	   (test-project-path)
+	   (let ((test-project
+		  (esy/project--of-path test-project-path)))
+	     (should (eq
+		      (esy/package-manager--of-project
+		       test-project)
+		      'npm))))
+   :teardown (lambda
+	       (x)
+	       (delete-directory x t))))
+
+(ert-deftest
+    test-esy/command-env--utils
+    ()
+  "command-env-* fns"
+  (ert/test-suite
+   :setup (lambda
+	    (tmp-dir)
+	    (let* ((test-project-path
+		   (esy-test-utils/fixture--create tmp-dir))
+		  (default-directory test-project-path))
+	      (progn
+		(shell-command "esy")
+		default-directory)))
+   :body (lambda
+	   (test-project-path)
+	   (let* ((test-project
+		   (esy/project--of-path test-project-path))
+		  (command-env
+		   (esy/command-env--of-project test-project))
+		  (penv
+		   (esy/command-env--to-process-environment
+		    command-env))
+		  (exec-path-list
+		   (esy/command-env--get-exec-path
+		    command-env)))
+	     (progn
+	       (should penv)
+	       (should (listp penv))
+	       (should (listp exec-path-list))
+	       (should penv)
+	       (should exec-path-list)
+	       (dolist (e penv)
+		 (progn
+		   (should (stringp e))
+		   (should (string-match "=" e)))))))
+   :teardown (lambda
+	       (x)
+	       (delete-directory x t))))
+
+(ert-deftest
+    test-esy/project--get-manifest-file-path
+    ()
+  "project--get-manifest-file-path must simply return the
+/path/to/manifest.json"
+  (ert/test-suite
+   :setup (lambda (tmp-dir)
+	    (esy-test-utils/fixture--create tmp-dir))
+   :body (lambda (test-project-path)
+	   (let* ((test-project
+		   (esy/project--of-path test-project-path)))
+	     (should
+	      (string=
+	       (esy/project--get-manifest-file-path
+		test-project)
+	       (concat
+		(file-name-as-directory
+		 (file-truename test-project-path))
+		"esy.json")))))
+   :teardown (lambda (x) (delete-directory x t))))
+		      
 (ert-deftest 
     test-esy/project--utils-for-a-valid-but-unsolved-project
     ()
   "Tests esy/project--* utils on trivial project (with no deps)"
-  (ert/test-suite :setup (lambda () 
-			   (let* ((tmp-dir (getenv "TMPDIR")) 
-				  (test-esy-project-dir (concat (file-name-as-directory tmp-dir)
-								"test-esy-project"))
-				  (test-esy-project-manifest (concat (file-name-as-directory test-esy-project-dir) "esy.json")))
-			     (progn (delete-directory test-esy-project-dir t)
-				    (make-directory test-esy-project-dir)
-				    (ert/f--write test-esy-project-manifest "{ \"dependencies\": {} }")
-				     test-esy-project-dir)))
-		  :body (lambda (fixture-project-path)
-			  (let* ((project (esy/project--of-path fixture-project-path)))
-							(should (esy/project--p project))
-							(should (not (esy/project--ready-p project)))))
-		  :tear-down (lambda (fixture-project-path)
-			       (delete-directory fixture-project-path t))))
+  (ert/test-suite
+   :setup (lambda (tmp-dir) 
+	    (let* ((test-esy-project-dir
+		    (concat
+		     (file-name-as-directory tmp-dir)
+		     "test-esy-project"))
+		   (test-esy-project-manifest
+		    (concat
+		     (file-name-as-directory
+		      test-esy-project-dir)
+		     "esy.json")))
+	      (progn 
+		(make-directory test-esy-project-dir)
+		(esy-test-utils/f--write
+		 test-esy-project-manifest
+		 "{ \"dependencies\": {} }")
+		test-esy-project-dir)))
+   :body (lambda (fixture-project-path)
+	   (let* ((project
+		   (esy/project--of-path
+		    fixture-project-path)))
+	     (should (esy/project--p project))
+	     (should (not (esy/project--ready-p project)))))
+   :teardown (lambda (fixture-project-path)
+	       (delete-directory fixture-project-path t))))
+
 (ert-deftest 
     test-esy/project--utils-for-a-reason-project
     ()
   "Tests esy/project--* utils on Reason project with refmt"
-  (ert/test-suite :setup (lambda () 
-			   (let* ((tmp-dir (getenv "TMPDIR")) 
-				  (test-esy-project-dir (concat (file-name-as-directory tmp-dir)
-								"test-esy-reason-project"))
-				  (test-esy-project-manifest (concat (file-name-as-directory test-esy-project-dir) "esy.json"))
-				  (test-opam-file (concat (file-name-as-directory test-esy-project-dir)
-							  "hello-reason.opam"))
-				  (test-helloml-file (concat (file-name-as-directory test-esy-project-dir)
-							  "hello.ml")))
-			     (progn (delete-directory test-esy-project-dir t)
-				    (make-directory test-esy-project-dir)
-				    (ert/f--write test-opam-file "")
-				    (ert/f--write test-esy-project-manifest "{ \"esy\": { \"buildInSource\": true, \"build\": \"dune build -name hello-reason\" }, \"dependencies\": { \"@opam/dune\": \"*\", \"@esy-ocaml/reason\": \"*\"} }")
-				    (ert/f--write test-helloml-file "let () = print_endline \"hello\"") test-esy-project-dir)))
-		  :body (lambda (fixture-project-path)
-			  (let* ((project (esy/project--of-path fixture-project-path)))
-							(should (esy/project--p project))
-							(should (not (esy/project--ready-p project)))))
-		  :tear-down (lambda (fixture-project-path) (delete-directory fixture-project-path t))))
+  (ert/test-suite
+   :setup (lambda (tmp-dir) 
+	    (let* ((test-esy-project-dir
+		    (concat (file-name-as-directory tmp-dir)
+			    "test-esy-reason-project"))
+		   (test-esy-project-manifest
+		    (concat
+		     (file-name-as-directory
+		      test-esy-project-dir)
+		     "esy.json"))
+		   (test-opam-file
+		    (concat (file-name-as-directory
+			     test-esy-project-dir)
+			    "hello-reason.opam"))
+		   (test-helloml-file
+		    (concat (file-name-as-directory
+			     test-esy-project-dir)
+			    "hello.ml")))
+	      (progn (delete-directory test-esy-project-dir t)
+		     (make-directory test-esy-project-dir)
+		     (esy-test-utils/f--write test-opam-file "")
+		     (esy-test-utils/f--write test-esy-project-manifest "{ \"esy\": { \"buildInSource\": true, \"build\": \"dune build -name hello-reason\" }, \"dependencies\": { \"@opam/dune\": \"*\", \"@esy-ocaml/reason\": \"*\"} }")
+		     (esy-test-utils/f--write test-helloml-file "let () = print_endline \"hello\"") test-esy-project-dir)))
+   :body (lambda (fixture-project-path)
+	   (let* ((project (esy/project--of-path fixture-project-path)))
+	     (should (esy/project--p project))
+	     (should (not (esy/project--ready-p project)))))
+   :teardown (lambda (fixture-project-path) (delete-directory fixture-project-path t))))
+
 (ert-deftest 
     test-esy/project--utils-for-an-ocaml-project
     ()
   "Tests esy/project--* utils on OCaml"
-  (ert/test-suite :setup (lambda () 
-			   (let* ((tmp-dir (getenv "TMPDIR")) 
-				  (test-esy-project-dir (concat (file-name-as-directory tmp-dir)
-								"test-esy-ocaml-project"))
-				  (test-esy-project-manifest (concat (file-name-as-directory test-esy-project-dir) "esy.json")))
-			     (progn (delete-directory test-esy-project-dir t)
-				    (make-directory test-esy-project-dir)
-				    (ert/f--write test-esy-project-manifest "{ \"esy\": { \"buildInSource\": true, \"build\": \"ocamlopt hello.ml\" }, \"dependencies\": { \"ocaml\": \"*\"} }")
-				    (ert/f--write "hello.ml" "let () = print_endline \"hello\"")
-				     test-esy-project-dir)))
-		  :body (lambda (fixture-project-path)
-			  (let* ((project (esy/project--of-path fixture-project-path)))
-							(should (esy/project--p project))
-							(should (not (esy/project--ready-p project)))))
-		  :tear-down (lambda (fixture-project-path)
-			       (delete-directory fixture-project-path t))))
+  (ert/test-suite
+   :setup (lambda (tmp-dir) 
+	    (let* ((test-esy-project-dir
+		    (concat (file-name-as-directory tmp-dir)
+			    "test-esy-ocaml-project"))
+		   (test-helloml-file
+		    (concat (file-name-as-directory
+			     test-esy-project-dir)
+			    "hello.ml"))
+		   (test-esy-project-manifest
+		    (concat
+		     (file-name-as-directory
+		      test-esy-project-dir)
+		     "esy.json")))
+	      (progn (delete-directory test-esy-project-dir t)
+		     (make-directory test-esy-project-dir)
+		     (esy-test-utils/f--write test-esy-project-manifest "{ \"esy\": { \"buildInSource\": true, \"build\": \"ocamlopt hello.ml\" }, \"dependencies\": { \"ocaml\": \"*\"} }")
+		     (esy-test-utils/f--write test-helloml-file "let () = print_endline \"hello\"")
+		     test-esy-project-dir)))
+   :body (lambda (fixture-project-path)
+	   (let* ((project
+		   (esy/project--of-path
+		    fixture-project-path)))
+	     (should (esy/project--p project))
+	     (should (not (esy/project--ready-p project)))))
+   :teardown (lambda (fixture-project-path)
+	       (delete-directory fixture-project-path t))))
+
 (ert-deftest 
     test-esy/project--utils-for-invalid-project
     ()
   "Tests esy/project--* utils on an project with empty manifest"
-  (ert/test-suite :setup (lambda () 
-			   (let* ((tmp-dir (getenv "TMPDIR")) 
-				  (test-esy-project-dir (concat (file-name-as-directory tmp-dir)
+  (ert/test-suite :setup (lambda (tmp-dir) 
+			   (let* ((test-esy-project-dir (concat (file-name-as-directory tmp-dir)
 								"test-esy-project-failing")))
 			     (progn (delete-directory test-esy-project-dir t)
 				    (make-directory test-esy-project-dir)
@@ -104,5 +313,5 @@
 			  (let* ((project (esy/project--of-path fixture-project-path)))
 			    (should (not (esy/project--p project)))
 			    (should (not (esy/project--ready-p project)))))
-		  :tear-down (lambda (fixture-project-path)
+		  :teardown (lambda (fixture-project-path)
 			       (delete-directory fixture-project-path t))))

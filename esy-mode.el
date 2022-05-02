f7403aac7cf454867981d97817474cd83d10d7b2
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2022-05-02 20:24:05 +0530

    Add convenient defuns with magit-style menus
    Load command-env regardless of isProjectReadyForDev
    But, run 'esy command-env' only after sources have been fetched
    
    It's useful to have command environment of an esy project loaded even
    if the entire project's sandbox hasn't been built. Tools like
    ocamllsp, merlin etc tend to have a relatively smaller dependency
    graph, making them ready for use early on. More importantly, these
    tools eventually come handy in fixing the project's dependencies (Ex: Ligo)
    
    esy/setup--esy fails if the project doesn't have its sources
    fetched because of 'esy command-env' - esy command-env would throw an
    error message (which isn't a json)
    
    To make things convenient, prompt the user
    to build the project
    
    defun: esy-view-build-logs and esy-view-build-dir
    Adds a new default esy-mode-callback
    It prints a info mesage
    
    defun: esy-install
    defun: esy-build
    
    Macros and esy-install-menu with repository options
    
    fix buffer name missing and use compilation-mode derived command
    output buffer
    
    Run run-cmd callback regardless of error/success
    
    Fix file loading regexp from compilation buffer
    
    Run esy commands in a special buffer
    
    Currently, the buffer is in compilation-mode
    TODO: esy-command-mode for the command buffers
    
    defun: esy b dune clean
    
    Lexical binding by default
    
    Fill initial value for defuns involving dependencies field of esy.json
    
    Defun: adds esy-build-dependencies

--- esy-mode.el
+++ esy-mode.el
@@ -1,4 +1,4 @@
-;;; esy.el --- Minor mode for esy, the package manager.
+;;; esy.el --- Minor mode for esy, the package manager. -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2020 Manas Jayanth
 
@@ -32,7 +32,7 @@
 (defvar esy-command "esy"
   "The 'esy' command. Can be full path to the esy binary.")
 
-(defvar esy-mode-callback (lambda (&optional project-type) nil)
+(defvar esy-mode-callback (lambda (&optional project-type) (message (format "%s project ready for development" project-type)))
   "The callback that can be run once an esy project is initialised.
 Common use case is to enable ask lsp client to connect to the server
 (since this can only be done after the esy project is ready)")
@@ -98,7 +98,12 @@ later be used to obtain more info about the esy project"
 (defun esy/project--of-buffer (buffer)
   "Returns an abstract structure that can
 later be used to obtain more info about the esy project"
-  (let* ((file-name (buffer-file-name buffer))) (if file-name (esy/project--of-file-path file-name) nil)))
+  (let* ((file-name (buffer-file-name buffer))) (if file-name (esy/project--of-file-path file-name) (esy/project--of-path default-directory))))
+
+(defun esy/project--fetched-p (project)
+  "Returns if a given project's sources have been solved and fetched. This
+is necessary for commands like 'esy command-env', 'esy build-plan' etc to work."
+  (gethash "isProjectFetched" (plist-get project 'json)))
 
 (defun esy/project--ready-p (project)
   "Returns if a given project is ready for
@@ -173,36 +178,38 @@ it looks for
 3. merlin
 
 "
-  (let* ((command-env (esy/command-env--of-project project))
-	(tools '()))
-    (progn
-      (setq process-environment
-	    (esy/command-env--to-process-environment
-	     command-env))
-      (setq exec-path
-	    (esy/command-env--get-exec-path command-env))
+  (let* ((tools '()))
       (setq tools (plist-put tools 'build "esy"))
       (setq tools (plist-put tools 'refmt (executable-find "refmt")))
       (setq tools (plist-put tools 'merlin (executable-find "ocamlmerlin")))
       (setq tools (plist-put tools 'lsp (executable-find "ocamllsp")))
-      tools)))
+  ))
 
 (defun esy/setup--esy (project callback)
   "setup--esy(project): runs ops to ensure project is ready
 for development"
+  (if (esy/project--fetched-p project)
+      (let* ((command-env (esy/command-env--of-project project)))
+	  (setq process-environment
+	    (esy/command-env--to-process-environment
+	     command-env))
+	  (setq exec-path
+		(esy/command-env--get-exec-path command-env)))
+    (if (y-or-n-p
+	 "This project hasn't had it's dependencies fetched and built. Go ahead and do this first?")
+	(run-esy
+	 (list "i")
+	 (lambda ()
+	   (message "Project dependencies have been fetched. Building sandbox in the background")
+	   (run-esy
+	    (list "build-dependencies")
+	    (lambda () (message "Project sandbox built!")))))))
   (if (esy/project--ready-p project)
       (progn
-	(message "Project ready for development")
+	(if (string= system-type "windows-nt")
+            (setq find-program "esy b find" grep-program "esy b grep"))
 	(funcall callback
 		 (esy/setup--esy-get-available-tools project)))
-    ;; (progn
-    ;;   (setq lexical-binding t)
-    ;;   (make-local-variable 'compilation-finish-functions)
-    ;;   (add-hook
-    ;;    'compilation-finish-functions
-    ;;    (lambda (buffer desc)
-    ;; 	 (funcall callback (esy/setup--esy-get-available-tools project))))
-    ;;   (compile "esy"))
     nil))
 
 (defun esy/setup--opam (project callback)
@@ -316,7 +323,7 @@ package.json or not"
 	    (esy/project--of-buffer (current-buffer)))))
     (esy/package-manager--of-project project)))
 
-(defun run-cmd (buffer-name cmd-and-args &optional callback) 
+(defun run-cmd-legacy (buffer-name cmd-and-args &optional callback)
   "Run the cmd" 
   (interactive) 
   (lexical-let ((callback-lex callback))
@@ -330,36 +337,175 @@ package.json or not"
 	(set-process-filter process 'comint-output-filter)) 
       (switch-to-buffer output-buffer-name))))
 
+(defun run-cmd (buffer-name cmd-and-args callback)
+  (let ((compilation-buffer
+		 (compilation-start (string-join cmd-and-args " ") 'compilation-mode)))
+    (if (get-buffer "*esy*") nil (with-current-buffer compilation-buffer (rename-buffer buffer-name)))
+    (add-hook
+     'compilation-finish-functions
+      (lambda (buf str)
+	(funcall callback)))))
+
+(defun esy/cmd-api (cmd-string)
+  "Util to work with esy's CLI API"
+  (string-trim
+   (shell-command-to-string cmd-string)))
+
 (defun esy-view-source (dependency)
   "Open dependency's source"
   (interactive "sDependency: ")
   (let* ((project (esy/project--of-buffer (current-buffer))))
     (if (esy/project--p project)
-	(find-file
-	 (string-trim
-	  (shell-command-to-string
-	   (concat esy-command (format " echo '#{%s.root}'" dependency)))))
+	(find-file (esy/cmd-api (format "%s -p %s echo #{%s.root}" esy-command dependency dependency)))
+      (message (format "Current buffer (%s) is not a part of an esy project" (buffer-name current-buffer))))))
+
+(defun esy-view-build-log (dependency)
+  "Open dependency's build logs"
+  (interactive (list (read-string "Dependency: " nil nil (concat "@" (thing-at-point 'symbol)))))
+  (let* ((project (esy/project--of-buffer (current-buffer))))
+    (if (esy/project--p project)
+	(find-file (esy/cmd-api (format "%s -p %s echo #{%s.target_dir}.log" esy-command dependency dependency)))
+      (message (format "Current buffer (%s) is not a part of an esy project" (buffer-name current-buffer))))))
+
+(defun esy-view-build-dir (&optional dependency)
+  "Opam a dependency's (if absent, root project's) build directory"
+  (interactive (list (read-string "Dependency: " nil nil (concat "@" (thing-at-point 'symbol)))))
+  (let* ((project (esy/project--of-buffer (current-buffer))))
+    (if (esy/project--p project)
+	(find-file (esy/cmd-api (format "%s -p %s echo #{%s.target_dir}" esy-command dependency dependency)))
       (message (format "Current buffer (%s) is not a part of an esy project" (buffer-name current-buffer))))))
 
 (defun esy-pesy ()
   "Run esy pesy"
   (interactive)
-  (run-cmd "*esy*" (list "esy" "pesy") (lambda () (message "[esy] Ran esy pesy"))))
+  (run-esy (list "pesy") (lambda () (message "[esy] Ran esy pesy"))))
+
+(defmacro plist-get-default (plist key default-value)
+ `(let ((val (plist-get ,plist ,key)))
+ (if val val ,default-value)))
+
+(defmacro append-if-t (arg plist args)
+  `(let ((val (plist-get ,plist ,arg)))
+     (if
+	 val
+	 (let
+	     ((arg-name (format "--%s" (string-trim (symbol-name ,arg) ":"))))
+	   (append ,args `(,arg-name ,val)))
+       ,args)))
+
+(defun esy-install-menu-arguments nil
+  "Arguments function for transient."
+  (transient-args 'esy/cmd-install))
+
+(defun esy/cmd-install (&optional args)
+  "Run esy install"
+  (interactive (list (esy-install-menu-arguments)))
+  (print args)
+  (run-esy (append '("install") args) (lambda () (message "[esy] Installed"))))
+
+(defun esy/cmd-build ()
+  "Run esy build"
+  (interactive)
+  (run-esy (list "build") (lambda () (message "[esy] Built"))))
 
 (defun esy-add (dependency &optional dev-only)
   "Run esy add <dependency>"
   (interactive "sDependency: ")
-  (run-cmd "*esy*" (list "esy" "add" dependency) (lambda () (message "[esy] Added"))))
+  (run-esy (list "add" dependency) (lambda () (message "[esy] Added"))))
 
-(defun esy ()
+(defun esy-build-and-install ()
   "Run esy"
   (interactive)
-  (run-cmd "*esy*" (list "esy") (lambda () (message "[esy] Finished"))))
+  (run-esy '() (lambda () (message "[esy] Build and install done"))))
 
-(defun esy-new (project-directory)
+(defun esy-npm-release ()
+  "Run esy npm-release"
+  (interactive)
+  (run-esy (list "npm-release") (lambda () (message "[esy]  NPM release done"))))
+
+(define-transient-command esy-install ()
+  "Open esy install transient menu pop up."
+    ["Arguments"
+     ("-p" "Package name providing the ocaml compiler"        "--ocaml-pkg-name=")
+     ("-v" "OCaml compiler version"        "--ocaml-version=")
+     ("-rl" "Local path to opam repository" "--opam-repository-local=")
+     ("-rr" "HTTP url to remote opam repository" "--opam-repository-remote=")
+     ("-ol" "Local path to opam override repository. For more info, see (TODO document this at esy.sh)" "--opam-override-repository-local=")
+     ("-or" "HTTP url to remote opam override repository. For more info, see (TODO document this at esy.sh)" "--opam-override-repository-remote=")
+    ]
+    [["Command"
+      ("i" "Install"       esy/cmd-install)]]
+  (interactive)
+  (transient-setup 'esy/cmd-install))
+
+(define-transient-command esy-build ()
+  "Open esy build transient menu pop up."
+    ["Arguments"
+     ("-p" "Package name providing the ocaml compiler"        "--ocaml-pkg-name")
+     ("-v" " OCaml compiler version"        "--ocaml-version")
+    ]
+    [["Command"
+      ("b" "Build"       esy-build)]]
+  (interactive)
+  (transient-setup 'esy/cmd-build))
+
+
+(defun esy-test ()
+  "Run esy test"
+  (interactive)
+  (run-esy (list "test") (lambda () (message "[esy] done"))))
+
+(defun esy-run-script (command)
+  "Run esy run-script. See https://esy.sh/docs/en/configuration.html#scripts"
+  (interactive)
+  (run-esy (list command) (lambda () (message "[esy] done"))))
+
+;; Entrypoint menu
+(define-transient-command esy-menu ()
+  "Open esy transient menu pop up."
+    [["Command"
+      ("e" "Build and install"       esy-build-and-install)
+      ("b" "Build"       esy-build)
+      ("i" "Install"       esy-install)
+      ("r" "Run Script"       esy-run-script)
+      ("n" "Run npm-release"       esy-npm-release)
+      ("t" "Test"       esy-test)
+    ]]
+  (interactive)
+  (transient-setup 'esy-menu))
+
+(defun esy ()
+  "Entrypoint function to the esy-mode interactive functions
+First checks if file backing the current buffer is a part of an esy project, then opens the menu. Else, recommends initialising a new project"
+  (interactive)
+  (let ((project (esy/project--of-buffer (current-buffer))))
+  (if project (call-interactively #'esy-menu) (if (y-or-n-p "You are not in an esy project, would you like to initialize one? ")
+          (call-interactively #'esy-init)))))
+
+(defun run-esy (args callback)
+  "Runs esy command in *esy* buffer"
+  (let ((command (if args (append args esy-command) (list esy-command))))
+  (run-cmd
+   "*esy*"
+   command
+   (lambda ()
+     (with-current-buffer
+	 "*esy*"
+       ; (make-local-variable 'compilation-directory-matcher) ; Buffer local not working :(
+       (setq
+	compilation-directory-matcher
+	'("^\s+\\(# esy-build-package: pwd: \\| esy-build-package: exiting with errors above\\)\\([^\n]+\\)$" (2 . nil)))
+       (callback))))))
+
+(defun esy-init (project-directory)
   "Run esy"
   (interactive "sProject Directory: ")
-  (run-cmd "*esy*" (list "pesy" "-d" project-directory) (lambda () (message "[esy] Finished"))))
+  (run-cmd (list "pesy" "-d" project-directory) (lambda () (message "[esy] Finished"))))
+
+(defun esy-dune-clean ()
+  "Cleanup dune's build directory"
+  (interactive)
+  (run-esy (list "b" "dune" "clean") (lambda () (message "[esy] Finished"))))
 
 (defun esy-project-is-ready?
     (file-path)
@@ -368,13 +514,17 @@ it returns if the project is ready for development"
   (let* ((project (esy/project--of-file-path file-path)))
     (esy/project--ready-p project)))
 
+(defun esy-build-dependencies (project-directory)
+  "Run esy build-dependencies"
+  (interactive)
+  (run-esy (list "build-dependencies") (lambda () (message "[esy] Finished"))))
+
 ;;;###autoload
 (define-minor-mode esy-mode
   "Minor mode for esy - the package manager for Reason/OCaml"
   :lighter " esy"
   (if esy-mode
   (progn
-    (setq lexical-binding t)
     (if (esy-mode-init)
     (let* ((project (esy/project--of-buffer (current-buffer))))
       (if (esy/project--p project)
